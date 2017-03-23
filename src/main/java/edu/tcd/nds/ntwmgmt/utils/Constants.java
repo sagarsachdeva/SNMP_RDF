c03ed33e7180fe3e9a83081953bb32546791e99a
Yana <kulizhsy@tcd.ie>
2017-03-23 12:11:12 +0000

    Fuseki pre-populated with data & Database name is displayed correctly

--- src/main/java/edu/tcd/nds/ntwmgmt/utils/Constants.java
+++ src/main/java/edu/tcd/nds/ntwmgmt/utils/Constants.java
@@ -10,6 +10,10 @@ public class Constants {
 	public static final int DB_PORT = 3030;
 
 	public static final String STORE_INFO_URL = "http://localhost:3030/$/server";
+	
+	public static final String STORE_DATA_URL = "http://localhost:3030/ds/data";
+	
+	public static final String RDF_DATA = "src/main/java/edu/tcd/nds/ntwmgmt/utils/pizza.rdf";
 
 	public static String[] agentEndpoints = { "udp:127.0.0.1/2001" };
 }
